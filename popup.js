9e3d0ea9c8f63a387f2d3f5d97a421d9b6fd2f31
Deepak Sharma <sharma.jnu@gmail.com>
2019-05-15 08:58:20 +0530

    QR Code implementation

--- popup.js
+++ popup.js
@@ -2,8 +2,20 @@ let url = '';
 
 const openInEdgeLink = document.getElementById('openInEdgeLink');
 
+
+
+var qrcode = new QRCode(document.getElementById("qrcode"), {
+	width : 100,
+	height : 100
+});
+
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
 
     openInEdgeLink.href = 'microsoft-edge:' + url;
+
+    qrcode.makeCode(url);
 });
+
+
+
