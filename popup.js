441450a771e7bcc38819e8f87a8822df8330ee17
Deepak Sharma <sharma.jnu@gmail.com>
2019-05-15 09:44:49 +0530

    Full functionality ready code cleanup and styling remaining

--- popup.js
+++ popup.js
@@ -1,4 +1,12 @@
-let url = '';
+
+
+const decisionsHosts = [
+    { name: 'Localhost', host: 'localhost', port: 44349 },
+    { name: 'Develop', host: 'devapp.boarddecisions.com' },
+    { name: 'Feature', host: 'decisionsfeature.azurewebsites.net' },
+    { name: 'Master', host: 'decisionsweb-main-test.azurewebsites.net' }
+];
+
 
 const openInEdgeLink = document.getElementById('openInEdgeLink');
 
@@ -9,12 +17,31 @@ var qrcode = new QRCode(document.getElementById("qrcode"), {
 	height : 150
 });
 
+
+const otherLinks = $('#otherLinks');
+
+
 chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
+    var urlStr = tabs[0].url;
 
-    openInEdgeLink.href = 'microsoft-edge:' + url;
+    openInEdgeLink.href = 'microsoft-edge:' + urlStr;
 
-    qrcode.makeCode(url);
+    const url = new URL(urlStr);
+
+    const elements = decisionsHosts.map(x => {
+
+        newUrl = new URL(urlStr.replace(url.hostname, x.host));
+        if(x.port) {
+            newUrl.port = x.port;
+        } else {
+            newUrl.port = 443;
+        }
+        return `<a class="block p-2 mt-2 hover:text-blue" style="border: solid 1px silver" href="${newUrl}" target="_blank">Open same page in ${x.name}</a>`
+    }).join(' ');
+
+
+    otherLinks.append(elements);
+    qrcode.makeCode(urlStr);
 });
 
 
