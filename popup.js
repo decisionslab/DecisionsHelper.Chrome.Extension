07bd5678b8149bdd3abdc3a16fe73a1e846ef637
Deepak Sharma <sharma.jnu@gmail.com>
2019-06-06 19:18:21 +0530

    dark mode

--- popup.js
+++ popup.js
@@ -1,5 +1,8 @@
 (function () {
 
+    const linkClasses = `block px-4 py-2 mt-2 text-center border-gray-400 text-gray-500 border border-gray-700 rounded hover:text-gray-100`;
+
+
     const openInEdgeLink = $('#openInEdgeLink');
     const otherLinks = $('#otherLinks');
     const qrCodeElement = $('#qrCode');
@@ -11,6 +14,14 @@
         openInEdgeLink.attr('href', 'microsoft-edge:' + urlStr);
 
         const host = (new URL(urlStr)).host;
+
+        var qrCode = new QRCode(qrCodeElement[0], {
+            width: 150,
+            height: 150,
+        });
+
+        qrCode.makeCode(urlStr);
+
         if (!window.decisionsData.allHosts.find(x => host.toLowerCase().indexOf(x.host.toLowerCase()) !== -1)) {
             generateDashboardUrls();
             return;
@@ -26,18 +37,13 @@
             } else {
                 newUrl.port = 443;
             }
-            return `<a class="block p-2 mt-2 text-center border-gray-400 text-gray-700 border border-2 hover:bg-blue-500 hover:text-white" href="${newUrl}" target="_blank">Switch to ${x.name}</a>`
+            return `<a class="${linkClasses}" href="${newUrl}" target="_blank">Switch to ${x.name}</a>`
         }).join(' ');
 
         $('#defaultImg').hide();
 
         otherLinks.append(elements);
-        var qrCode = new QRCode(qrCodeElement[0], {
-            width: 150,
-            height: 150
-        });
 
-        qrCode.makeCode(urlStr);
 
     }
 
@@ -52,13 +58,13 @@
     function generateDashboardUrls() {
 
         $('#envHeaderTxt').text('Dashboards:');
-        qrCodeElement.hide();
-        $('#defaultImg').show();
+        // qrCodeElement.hide();
+        // $('#defaultImg').show();
 
         const elements = window.decisionsData.allHosts.map(x => {
 
             newUrl = new URL(`https://${x.host}:${x.port ? x.port : 443}/web/#/dashboard`);
-            return `<a class="block p-2 mt-2 text-center border-gray-400 text-gray-700 border border-2 hover:bg-blue-500 hover:text-white" href="${newUrl}" target="_blank">${x.name} Dashboard</a>`
+            return `<a class="${linkClasses}" href="${newUrl}" target="_blank">${x.name} Dashboard</a>`
         }).join(' ');
 
         otherLinks.append(elements);
