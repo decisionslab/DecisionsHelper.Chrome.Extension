23d9484758ca8c5cca38f2b29591de4540eefcd2
Deepak Sharma <sharma.jnu@gmail.com>
2019-12-31 15:50:05 +0530

    Using new hosts

--- common.js
+++ common.js
@@ -7,8 +7,8 @@
     window.decisionsData.allHosts = [
         { name: 'Localhost', host: 'localhost', port: 44349 },
         { name: 'Develop', host: 'devapp.boarddecisions.com' },
-        { name: 'Feature', host: 'decisionsfeature.azurewebsites.net' },
-        { name: 'Master', host: 'decisionsweb-main-test.azurewebsites.net' }
+        { name: 'Feature', host: 'feature.boarddecisions.com' },
+        { name: 'Master', host: 'master.boarddecisions.com' }
     ];
 
     window.browser = window.browser || window.chrome;
